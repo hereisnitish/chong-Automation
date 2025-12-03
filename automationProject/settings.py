e72700df927a77eece7292d615fc04255dbb7622
rahul-snakescript <113518994+rahul-snakescript@users.noreply.github.com>
2025-12-03 16:11:27 +0530

    Update settings.py

--- automationProject/settings.py
+++ automationProject/settings.py
@@ -137,8 +137,8 @@ AUTH_USER_MODEL = 'automationApp.User'
 # Email Configuration (Gmail SMTP)
 # ----------------------------------
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 
